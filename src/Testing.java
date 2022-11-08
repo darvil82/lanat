7871bee1c5753a44b1837d4de0681d6a9a1e295a
DarviL82 <davidlosantos89@gmail.com>
2022-11-08 21:35:18 +0100

    add file

---
+++ src/Testing.java
@@ -0,0 +1,26 @@
+import argparser.Argument;
+import argparser.ArgumentType;
+import argparser.Command;
+
+public class Testing {
+	public static void main(String[] args) {
+		var argParser = new TestingParser("SimpleTesting") {{
+			addArgument(new Argument<>("what", new StringJoiner())
+				.callback(t -> System.out.println("wow look a string: '" + t + "'"))
+				.positional()
+				.obligatory()
+				.errorCode(0x01)
+			);
+			addArgument(new Argument<>("a", ArgumentType.BOOLEAN()));
+			addSubCommand(new Command("subcommand") {{
+				addArgument(new Argument<>("c", ArgumentType.COUNTER()));
+				addArgument(new Argument<>('s', "more-strings", new StringJoiner()));
+				addSubCommand(new Command("another") {{
+					addArgument(new Argument<>("ball", new StringJoiner()));
+					addArgument(new Argument<>("number", ArgumentType.INTEGER()).positional().obligatory());
+				}});
+			}});
+		}};
+		argParser.parseArgs("12 subcommand another --foobar");
+	}
+}

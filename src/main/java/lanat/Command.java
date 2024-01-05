42b2d35ea1e8e59d50fb99b89443d349b2b54acf
darvil82 <davidlosantos89@gmail.com>
2024-01-05 15:25:52 +0100

    fix callbacks running twice

--- src/main/java/lanat/Command.java
+++ src/main/java/lanat/Command.java
@@ -125,7 +125,7 @@ public class Command
 		this.addArgument(Argument.createOfBoolType("help", "h")
 			.onOk(t -> {
 				System.out.println(this.getHelp());
-//				System.exit(0);
+				System.exit(0);
 			})
 			.withDescription("Shows this message.")
 			.allowsUnique()

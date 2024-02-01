052414ceff966e99365e3802facf4925cc370f8c
darvil82 <davidlosantos89@gmail.com>
2024-02-01 17:52:08 +0100

    minor tweaks to more types

--- src/main/java/lanat/argumentTypes/MultipleNumbersArgumentType.java
+++ src/main/java/lanat/argumentTypes/MultipleNumbersArgumentType.java
@@ -12,14 +12,14 @@ import utils.Range;
 public class MultipleNumbersArgumentType<TInner extends Number> extends TupleArgumentType<TInner> {
 	/**
 	 * Creates a new {@link TupleArgumentType} with the specified range of values that the argument will take.
-	 * @param range The range of values that the argument will take.
+	 * @param valueCount The range of values that the argument will take.
 	 * @param defaultValue The default value of the argument. This will be used if no values are provided.
 	 * @throws lanat.exceptions.ArgumentTypeInferException If the type of the default value is not supported.
 	 */
 	@SuppressWarnings("unchecked")
-	public MultipleNumbersArgumentType(@NotNull Range range, @NotNull TInner[] defaultValue) {
+	public MultipleNumbersArgumentType(@NotNull Range valueCount, @NotNull TInner[] defaultValue) {
 		super(
-			range,
+			valueCount,
 			// we can infer the type of the argument type from the default value
 			(ArgumentType<TInner>)ArgumentTypeInfer.get(defaultValue.getClass().getComponentType()),
 			defaultValue

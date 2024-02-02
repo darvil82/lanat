cdd81e7300f5f3b909f7ee0a02256efdac8396d6
darvil82 <davidlosantos89@gmail.com>
2024-02-02 02:40:22 +0100

    make StringArgumentType final to prevent possible bugs with custom types inheriting it

--- src/main/java/lanat/argumentTypes/StringArgumentType.java
+++ src/main/java/lanat/argumentTypes/StringArgumentType.java
@@ -2,14 +2,13 @@ package lanat.argumentTypes;
 
 import lanat.ArgumentType;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import textFormatter.TextFormatter;
 
 /**
  * An argument type that takes a string of characters.
  * @see String
  */
-public class StringArgumentType extends ArgumentType<String> {
+public final class StringArgumentType extends ArgumentType<String> {
 	@Override
 	public String parseValues(@NotNull String @NotNull [] args) {
 		return args[0];
@@ -21,7 +20,7 @@ public class StringArgumentType extends ArgumentType<String> {
 	}
 
 	@Override
-	public @Nullable String getDescription() {
+	public @NotNull String getDescription() {
 		return "A string of characters.";
 	}
 }

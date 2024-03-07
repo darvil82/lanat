7ab1fee2a0163f36ef2d72210bd2735872142271
darvil82 <davidlosantos89@gmail.com>
2024-03-07 16:17:20 +0100

    add another tuple test

--- src/test/java/lanat/test/units/TestTuples.java
+++ src/test/java/lanat/test/units/TestTuples.java
@@ -1,5 +1,6 @@
 package lanat.test.units;
 
+import lanat.TupleChar;
 import lanat.test.UnitTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,15 @@ public class TestTuples extends UnitTests {
 		assertEquals("(hello), (world)", this.parseArg("what", "['hello' 'world']"));
 		assertEquals("(hello)", this.parser.parseGetValues("-what=['hello']").get("what").orElse(null));
 		assertEquals("(hello), (world)", this.parser.parseGetValues("-what=['hello' world]").get("what").orElse(null));
+	}
+
+	@Test
+	@DisplayName("Test other tuple chars")
+	public void testOtherTupleChars() {
+		TupleChar.current = TupleChar.PARENTHESIS;
+		assertEquals("(multiple), (values), (here)", this.parseArg("what", "(multiple values here)"));
+		TupleChar.current = TupleChar.ANGLE_BRACKETS;
+		assertEquals("(multiple), (values), (here)", this.parseArg("what", "<multiple values here>"));
+		TupleChar.current = TupleChar.SQUARE_BRACKETS; // reset
 	}
 }

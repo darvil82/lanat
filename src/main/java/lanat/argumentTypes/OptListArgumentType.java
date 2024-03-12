598c9d4e0f25bd83780df5811e950a0cb21fe767
darvil82 <davidlosantos89@gmail.com>
2024-03-12 11:54:16 +0100

    add: missing annotations fix: jetbrains annotations are now a compileOnly dependency

--- src/main/java/lanat/argumentTypes/OptListArgumentType.java
+++ src/main/java/lanat/argumentTypes/OptListArgumentType.java
@@ -4,7 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+/**
+ * An argument type that restricts a possible value out of a list of values.
+ */
 public class OptListArgumentType extends SingleValueListArgumentType<String> {
+	/**
+	 * Creates a new optional list argument type.
+	 * @param values The list of values that the argument type will accept.
+	 * @param initialValue The initial value of the argument type. This value must be one of the values.
+	 */
 	public OptListArgumentType(@NotNull List<String> values, @NotNull String initialValue) {
 		super(values.toArray(String[]::new), initialValue);
 
@@ -12,10 +20,18 @@ public class OptListArgumentType extends SingleValueListArgumentType<String> {
 			throw new IllegalArgumentException("Initial value must be one of the values.");
 	}
 
+	/**
+	 * Creates a new optional list argument type.
+	 * @param values The list of values that the argument type will accept.
+	 */
 	public OptListArgumentType(@NotNull List<String> values) {
 		super(values.toArray(String[]::new));
 	}
 
+	/**
+	 * Creates a new optional list argument type.
+	 * @param values The list of values that the argument type will accept.
+	 */
 	public OptListArgumentType(@NotNull String... values) {
 		super(values);
 	}

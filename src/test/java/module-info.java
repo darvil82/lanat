ba6add7e0b397c2da484a6ac85a610569ff189ee
darvil82 <davidlosantos89@gmail.com>
2025-11-13 18:56:23 +0100

    fix tests module info not importing correctly

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,11 +1,11 @@
-module lanat.test {
+module darvil.lanat.test {
 	requires org.junit.jupiter.api;
-	requires lanat;
+	requires darvil.lanat;
 	requires org.jetbrains.annotations;
 	requires utils;
 	requires textFormatter;
 
-	exports lanat.tests to lanat, org.junit.platform.commons, utils;
-	exports lanat.tests.parser.commandTemplates to lanat, org.junit.platform.commons, utils;
-	exports lanat.tests.parser to lanat, org.junit.platform.commons, utils;
+	exports lanat.tests to darvil.lanat, org.junit.platform.commons, utils;
+	exports lanat.tests.parser.commandTemplates to darvil.lanat, org.junit.platform.commons, utils;
+	exports lanat.tests.parser to darvil.lanat, org.junit.platform.commons, utils;
 }
